9d2e5abba47ca7f4dc85c0c47468154fe40ae72a
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-07-24 14:18:23 +0200

    dark-mode side end, add language hover and focus, add night mode button toggle and border on hover

--- landing-page/index.js
+++ landing-page/index.js
@@ -1,16 +1,21 @@
-let tl = gsap.timeline({ repeat: -1, yoyo: true});
+// INIZIO JS SECTION 7 - FRANCESCO
 
-let animazione = tl.to(".pancakeLoop1", {y: 25, x: 18, duration: 1.5, stagger: 0.2, ease: "power0.inOut"})
-    .to(".pancakeLoop2", {y: 35, x: 25, duration: 1.5, stagger: 0.2, ease: "power0.inOut"}, "-=1.5")
+gsap.to(".pancakeLoop1", {repeat: -1, yoyo: true, y: 22, x: 12, duration: 1.5, ease: "power1.inOut"})
+gsap.to(".pancakeLoop2", {repeat: -1, yoyo: true, y: 24, x: 18, duration: 1.5, ease: "power1.inOut", delay: 0.1} )
+gsap.to(".pancakeLoop3", {repeat: -1, yoyo: true, y: 27, x: 15, duration: 1.5, ease: "power1.inOut", delay: 0.3})
+gsap.to(".pancakeLoop4", {repeat: -1, yoyo: true, y: 22, x: 12, duration: 1.5, ease: "power1.inOut"})
+gsap.to(".pancakeLoop5", {repeat: -1, yoyo: true, y: 24, x: 18, duration: 1.5, ease: "power1.inOut", delay: 0.1} )
+gsap.to(".pancakeLoop6", {repeat: -1, yoyo: true, y: 27, x: 15, duration: 1.5, ease: "power1.inOut", delay: 0.3})
 
 document.querySelector("section.sectionSeven > div.sectionSeven-centralDiv > div:nth-child(4) > button").addEventListener('mouseover', ()=>{
     gsap.to("section.sectionSeven > div.sectionSeven-centralDiv > div:nth-child(4) > button",{opacity: 0.7, duration: 0.2})
     document.querySelector("section.sectionSeven > div.sectionSeven-centralDiv > div:nth-child(4) > button").addEventListener("mouseout",()=> {
         gsap.to("section.sectionSeven > div.sectionSeven-centralDiv > div:nth-child(4) > button",{opacity: 1, duration: 0.2})})
 
-})
+    })
+// FINE JS SECTION 7 - FRANCESCO
 
-
+// INIZIO JS FOOTER - FRANCESCO
 document.querySelector("div.bottomFooterRight > div.rightSide > a > button").addEventListener("mouseover", () =>{
     gsap.to("div.bottomFooterRight > div.rightSide > a > button", {opacity: 0.7, duration: 0.2});
     document.querySelector("div.bottomFooterRight > div.rightSide > a > button").addEventListener('mouseout',()=>{
@@ -26,20 +31,34 @@ document.querySelector("div.bottomFooterRight > div.leftSide > a").addEventListe
 })
 
 
-// document.querySelector("div.bottomFooterLeft > div.rightSide").addEventListener('mouseover',(ev)=>{
-//     ev.stopPropagation();
-//     gsap.to("div.bottomFooterLeft > div.rightSide",{opacity:0.6, duration: 0.3})
-//     document.querySelector("div.bottomFooterLeft > div.rightSide").addEventListener('mouseleave', (ev)=>{
-//         ev.stopPropagation();
-//         gsap.to("div.bottomFooterLeft > div.rightSide",{opacity:1, duration: 0.2})
-//         })})
-
-document.querySelector("div.bottomFooterLeft > div.rightSide > div.showedSide").addEventListener('click',(ev)=>{
-    ev.stopPropagation();
-    document.querySelector("div.bottomFooterLeft > div.rightSide > div.languages").style.display = "block"
-    gsap.fromTo("div.bottomFooterLeft > div.rightSide > div.languages",{opacity:0}, {opacity: 1, duration: 0.5})
-    document.querySelector("html").addEventListener('click', async ()=>{
-       await  gsap.fromTo("div.bottomFooterLeft > div.rightSide > div.languages",{opacity:1}, {opacity: 0, duration: 0.5})
-        document.querySelector("div.bottomFooterLeft > div.rightSide > div.languages").style.display = "none"
-    })
+window.addEventListener("DOMContentLoaded",async()=>{
+    let fetched = await fetch("https://api.pancakeswap.info/api/v2/tokens");
+    let response = await fetched.json();
+    let price = await response.data["0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"].price
+    numero = Number(price).toFixed(3);
+    console.log(numero)
+    let selettore = document.querySelector("div.bottomFooterRight > div.leftSide > a > div");
+    selettore.textContent = `$${numero}`;
+    
+    setInterval(async ()=>{
+    let fetched = await fetch("https://api.pancakeswap.info/api/v2/tokens");
+    let response = await fetched.json();
+    let price = await response.data["0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"].price
+    numero = Number(price).toFixed(3);
+    console.log(numero)
+    let selettore = document.querySelector("div.bottomFooterRight > div.leftSide > a > div");
+    selettore.textContent = `$${numero}`;
+    }, 1000);
 })
+
+document.querySelector('div.bottomFooterLeft > div.leftSide').addEventListener('click', ()=>{
+    if(document.querySelector('body').classList.contains('dark-mode')){
+        document.querySelector('body').classList.remove('dark-mode')
+        gsap.to('div.leftSide > div.slider', {x: 15, duration: 0.3})
+    }
+    else{
+        document.querySelector('body').classList.add('dark-mode')
+         gsap.to('div.leftSide > div.slider', {x: -14, duration: 0.3})
+    }
+})
+// FINE JS FOOTER - FRANCESCO

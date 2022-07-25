cbb0f9447380346980eb30f9cbda99dc767da7c8
Alessio Valdarchi <Valdanito994@gmail.com>
2022-07-25 16:49:12 +0200

    fixed js func

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ let animazione = tl.to("#main-img", { y: 25, x: 18, duration: 1.5, stagger: 0.2,
 async function getApi() {
     let response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=150&page=1&sparkline=false&price_change_percentage=10s')
     let fetchApi = await response.json()
-    //console.log(fetchApi);
-    let pancakeData = fetchApi[93];
+    let pancakeData = fetchApi.find(elem => elem.id === 'pancakeswap-token')
     console.log(pancakeData);
     return pancakeData;
 }
 
-let pancakeDataKeys = ['total_supply', 'circulating_supply', 'market_cap'];
+let pancakeDataKeys = ['total_supply', 'circulating_supply'];
 function updateData() {
     getApi().then(pancakeData => {
         for (let key of pancakeDataKeys) {
@@ -27,6 +26,9 @@ function updateData() {
             else
                 console.log('Error! no dom elem with this id: ' + key);
         }
+        let marketCap = document.getElementById('market_cap');
+        marketCap.innerHTML = `$${Math.round(pancakeData.market_cap)
+            .toString().substring(0, 3)} million`
 
     })
 }

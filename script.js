7eeedddd099157eead390c6b4ff19dfdd71e55f7
FabioAranzulla <fabio.aranzulla88@gmail.com>
2022-07-28 11:21:41 +0200

    add hidden div

--- script.js
+++ script.js
@@ -26,48 +26,14 @@ let animazione = tl
 
 const title = document.querySelector(".title-res-data>span");
 const arrows = document.querySelector(".title-res-data>button");
-const prova = document.querySelector(".res-data");
+const hidden = document.querySelector(".hidden");
 
 arrows.addEventListener("click", () => {
   if (title.innerHTML === "Syrup Pools") {
     title.innerHTML = "Farms";
+    hidden.style.display = "none";
   } else if (title.innerHTML === "Farms") {
     title.innerHTML = "Syrup Pools";
+    hidden.style.display = "block";
   }
 });
-
-// const swap = `<div id="sub-res-data">
-// <h6>ciao</h6>
-// <p>195.496%</p>
-// <p>APR</p>
-// </div>
-
-// <div id="subtitle-res-data">
-// <h6>TRIVIA-BNB LP</h6>
-// <p>178.866%</p>
-// <p>APR</p>
-// </div>
-
-// <div id="subtitle-res-data">
-// <h6>BTCST-BNB LP</h6>
-// <p>117.474%</p>
-// <p>APR</p>
-// </div>
-
-// <div id="subtitle-res-data">
-// <h6>BTCST-BNB LP</h6>
-// <p>117.474%</p>
-// <p>APR</p>
-// </div>
-
-// <div id="subtitle-res-data">
-// <h6>SDAO-BUSD LP</h6>
-// <p>92.448%</p>
-// <p>APR</p>
-// </div>
-
-// <div id="subtitle-res-data">
-// <h6>DUET-CAKE LP</h6>
-// <p>90.325%</p>
-// <p>APR</p>
-// </div>`;

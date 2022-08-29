2a321fe8d7abbac131b7644d0e3f47692e07443a
ceci <ceciliafabiole18@gmail.com>
2022-08-29 19:56:04 +0300

    fix: responsive, javascript

--- section_3-5.js
+++ section_3-5.js
@@ -1,30 +1,42 @@
-//Animaazioni monete Sezione 3
+//Animazioni monete Sezione 3
 gsap.to("#coin1", {repeat: -1, yoyo: true, y: 40, x: 12, duration: 2, ease: "power1.inOut"})
 gsap.to("#coin2", {repeat: -1, yoyo: true, y: 24, x: 18, duration: 2, ease: "power1.inOut", delay: 0.1} )
 gsap.to("#coin3", {repeat: -1, yoyo: true, y: 22, x: 15, duration: 2, ease: "power1.inOut", delay: 0.3})
 
 //Animazioni carte e palline da biliardo Sezione 5
-gsap.to("#m-c-1", {repeat: -1, yoyo: true, y: 27, x: 2, duration: 2, ease: "power1.inOut"})
-gsap.to("#m-c-2", {repeat: -1, yoyo: true, y: 22, x: 16, duration: 2, ease: "power1.inOut", delay: 0.1} )
-gsap.to("#m-c-3", {repeat: -1, yoyo: true, y: 10, x: 15, duration: 2.5, ease: "power1.inOut", delay: 0.3})
-gsap.to("#m-c-4", {repeat: -1, yoyo: true, y: 15, x: 12, duration: 1.5, ease: "power1.inOut"})
-gsap.to("#b-b-1", {repeat: -1, yoyo: true, y: 10, x: 12, duration: 2, ease: "power1.inOut", delay: 0.1} )
-gsap.to("#b-b-2", {repeat: -1, yoyo: true, y: 15, x: 15, duration: 2, ease: "power1.inOut", delay: 0.3})
-gsap.to("#b-b-3", {repeat: -1, yoyo: true, y: 10, x: 20, duration: 2.5, ease: "power1.inOut"})
-gsap.to("#b-b-4", {repeat: -1, yoyo: true, y: 24, x: 10, duration: 2, ease: "power1.inOut", delay: 0.1} )
+gsap.to("#m-c-1", {repeat: -1, yoyo: true, y: 27, x: 0, duration: 2, ease: "power1.inOut"})
+gsap.to("#m-c-2", {repeat: -1, yoyo: true, y: 22, x: 22, duration: 2, ease: "power1.inOut", delay: 0.1} )
+gsap.to("#m-c-3", {repeat: -1, yoyo: true, y: 10, x: 12, duration: 2.5, ease: "power1.inOut", delay: 0.3})
+gsap.to("#m-c-4", {repeat: -1, yoyo: true, y: 15, x: 18, duration: 1.5, ease: "power1.inOut"})
+gsap.to("#b-b-1", {repeat: -1, yoyo: true, y: 18, x: -6, duration: 1.5, ease: "power1.inOut", delay: 0.1} )
+gsap.to("#b-b-2", {repeat: -1, yoyo: true, y: 15, x: -12, duration: 2.5, ease: "power1.inOut", delay: 0.3})
+gsap.to("#b-b-3", {repeat: -1, yoyo: true, y: 10, x: -8, duration: 2, ease: "power1.inOut"})
+gsap.to("#b-b-4", {repeat: -1, yoyo: true, y: 24, x: 0, duration: 2, ease: "power1.inOut", delay: 0.1} )
 
-//Funzioni aggiornare dati Sezione 5
-const jackpotPrediction = document.getElementById('jackpot-prediction');
+//Funzioni per aggiornare dati Sezione 5
 const jackpotLottery = document.getElementById('jackpot-lottery');
 
-let predictionAmount = 198;
-setInterval(() => {
-    predictionAmount += 1;
-    jackpotPrediction.innerHTML = `$${predictionAmount} billion`;
-}, 1000);
 
-let lotteryAmount = 1345;
-setInterval(() => {
-    lotteryAmount += 1;
-    jackpotLottery.innerHTML = `$${lotteryAmount}`;
-}, 1000);
+
+const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    let internationalNumberFormat = new Intl.NumberFormat('en-US')
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        target.innerText = '$' + internationalNumberFormat.format(Math.floor(progress * (end - start) + start));
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+};
+
+function getCounter(){
+    jackpotLottery.innerHTML = counterAnim('#jackpot-lottery', 78000, 90000, 20000);
+}
+getCounter();
+
+
+setInterval(getCounter, 30000)

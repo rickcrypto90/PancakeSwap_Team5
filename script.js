4537c32d03b19f9b682f573fef8fa3d3e4dd19df
FabioAranzulla <fabio.aranzulla88@gmail.com>
2022-08-31 19:20:59 +0200

    fix responsive

--- script.js
+++ script.js
@@ -8,16 +8,8 @@ let animazione = tl
     stagger: 0.2,
     ease: "inOut",
   })
-  .to(
-    "#arrow",
-    { y: 10, x: 5, duration: 1, stagger: 0.2, ease: "in" },
-    "-=1.5"
-  )
-  .to(
-    "#cake",
-    { y: 15, x: 5, duration: 1, stagger: 0.2, ease: "in" },
-    "-=1.5"
-  )
+  .to("#arrow", { y: 10, x: 5, duration: 1, stagger: 0.2, ease: "in" }, "-=1.5")
+  .to("#cake", { y: 15, x: 5, duration: 1, stagger: 0.2, ease: "in" }, "-=1.5")
   .to(
     "#folder",
     { y: 15, x: 5, duration: 1, stagger: 0.2, ease: "in" },
@@ -26,17 +18,23 @@ let animazione = tl
 
 const title = document.querySelector(".title-res-data>span");
 const arrows = document.querySelector(".title-res-data>button");
-const hidden = document.querySelector(".hidden");
+const hidden = document.querySelector("#res-data-hidden");
 const first = document.querySelector(".res-data");
 
 arrows.addEventListener("click", () => {
   if (title.innerHTML === "Farms") {
     title.innerHTML = "Syrup Pools";
-    hidden.style.display = "block";
-    first.style.display = "none";
+    hidden.style.display = "flex";
+    // first.style.display = "none";
+    first.setAttribute("style", "display:none !important");
   } else if (title.innerHTML === "Syrup Pools") {
     title.innerHTML = "Farms";
     hidden.style.display = "none";
     first.style.display = "flex";
+    hidden.setAttribute("style", "display:none !important");
   }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  hidden.setAttribute("style", "display:none !important");
 });
